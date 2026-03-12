62b2000bfd16b9c2a86a8f0d0e94f212239da164
Combxzzz <pizzivinicius@gmail.com>
2026-03-12 09:19:34 -0300

    feat(BankAccountRepository): Creating bank account repository

--- src/main/java/repository/BankAccountRepository.java
+++ src/main/java/repository/BankAccountRepository.java
@@ -1,4 +1,31 @@
 package repository;
 
-public class BankAccountRepository {
+import model.BankAccount;
+
+import java.util.*;
+
+                                                                      // Autoboxing: int -> Integer
+public class BankAccountRepository implements Repository<BankAccount, Integer> {
+    private final Map<Integer, BankAccount> bankAccountMap = new HashMap<>();
+
+    @Override
+    public BankAccount save(BankAccount entity) {
+        bankAccountMap.put(entity.getAccountId(), entity);
+        return entity;
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        bankAccountMap.remove(id);
+    }
+
+    @Override
+    public List<BankAccount> findAll() {
+        return new ArrayList<>(bankAccountMap.values());
+    }
+
+    @Override
+    public Optional<BankAccount> findById(Integer id) {
+        return Optional.ofNullable(bankAccountMap.get(id));
+    }
 }
