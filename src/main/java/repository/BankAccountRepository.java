bb72cbdd61e1b55fbf2fcd851c02bcf74d4cd80e
Combxzzz <pizzivinicius@gmail.com>
2026-03-12 09:40:55 -0300

    refactor(BankAccountRepository): Add abstract repository implementation to bank account repository

--- src/main/java/repository/BankAccountRepository.java
+++ src/main/java/repository/BankAccountRepository.java
@@ -2,30 +2,10 @@ package repository;
 
 import model.BankAccount;
 
-import java.util.*;
-
-                                                                      // Autoboxing: int -> Integer
-public class BankAccountRepository implements Repository<BankAccount, Integer> {
-    private final Map<Integer, BankAccount> bankAccountMap = new HashMap<>();
-
+                                                                           // Autoboxing: int -> Integer
+public class BankAccountRepository extends AbstractRepository<BankAccount, Integer>{
     @Override
-    public BankAccount save(BankAccount entity) {
-        bankAccountMap.put(entity.getAccountId(), entity);
-        return entity;
-    }
-
-    @Override
-    public void deleteById(Integer id) {
-        bankAccountMap.remove(id);
-    }
-
-    @Override
-    public List<BankAccount> findAll() {
-        return new ArrayList<>(bankAccountMap.values());
-    }
-
-    @Override
-    public Optional<BankAccount> findById(Integer id) {
-        return Optional.ofNullable(bankAccountMap.get(id));
+    protected Integer getId(BankAccount entity) {
+        return entity.getAccountId();
     }
 }
